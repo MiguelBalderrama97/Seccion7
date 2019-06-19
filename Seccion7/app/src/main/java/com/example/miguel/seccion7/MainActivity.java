bd26f52b52b7c18712a98336a7a9b23c5368a6f3
MiguelBalderrama97 <37118731+MiguelBalderrama97@users.noreply.github.com>
2019-06-19 15:31:49 -0600

    Implementación Toolbar

--- Seccion7/app/src/main/java/com/example/miguel/seccion7/MainActivity.java
+++ Seccion7/app/src/main/java/com/example/miguel/seccion7/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.miguel.seccion7;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar myToolbar;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        
     }
 }
